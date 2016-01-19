bc795bfe30b88d6a543217c252e596feb2d2699e
John Hogan <johnehogan@yahoo.com>
2016-01-18 21:51:08 -0500

    Updated van routes page

--- app/controllers/van_routes_controller.rb
+++ app/controllers/van_routes_controller.rb
@@ -1,11 +1,81 @@
 class VanRoutesController < ApplicationController
-  def index
-    @van_route = VanRoute.find_by route_date: DateTime.now.to_date
-    if @van_route.nil?
-      @van_route = VanRoute.new(route_date: DateTime.now.utc, am_pm: "AM", van_id: Van.first.id, driver_id: Driver.first.id)
-      @van_route.route_stops.build
+before_action :set_van_route, only: [:show, :edit, :update, :destroy]
+
+# GET /van_routes
+# GET /van_routes.json
+def index
+  @van_routes = VanRoute.all
+end
+
+# GET /van_routes/1
+# GET /van_routes/1.json
+def show
+end
+
+# GET /van_routes/new
+def new
+  @van_route = VanRoute.new(route_date: DateTime.now.utc, am_pm: 'AM', van_id: Van.first.id, driver_id: Driver.first.id)
+  @van_route.route_stops.build
+end
+
+# GET /van_routes/1/edit
+def edit
+end
+
+# POST /van_routes
+# POST /van_routes.json
+def create
+  @van_route = VanRoute.new(van_route_params)
+
+  respond_to do |format|
+    if @van_route.save
+      format.html { redirect_to @van_route, notice: 'Van route was successfully created.' }
+      format.json { render :show, status: :created, location: @van_route }
     else
-      @van_route.route_date = Time.zone.parse(@van_route.route_date).utc
+      format.html { render :new }
+      format.json { render json: @van_route.errors, status: :unprocessable_entity }
     end
   end
+end
+
+# PATCH/PUT /van_routes/1
+# PATCH/PUT /van_routes/1.json
+def update
+  respond_to do |format|
+    if @van_route.update(van_route_params)
+      format.html { redirect_to @van_route, notice: 'Van route was successfully updated.' }
+      format.json { render :show, status: :ok, location: @van_route }
+    else
+      format.html { render :edit }
+      format.json { render json: @van_route.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+# DELETE /van_routes/1
+# DELETE /van_routes/1.json
+def destroy
+  @van_route.destroy
+  respond_to do |format|
+    format.html { redirect_to van_routes_url, notice: 'Van route was successfully destroyed.' }
+    format.json { head :no_content }
+  end
+end
+
+def org
+  @van_route = VanRoute.find(params[:id])
+  #add the data to an array for JSON formatting purposes
+  @assignment_array = Array.new(1, @van_route)
+end
+
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_van_route
+  @van_route = VanRoute.find(params[:id])
+end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+def van_route_params
+  params.require(:van_route).permit(:name, :route_date, :am_pm, :van_id, :driver_id)
+end
 end
