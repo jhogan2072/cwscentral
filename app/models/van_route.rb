779bf50bb5d92e537aceb444138aedb2e6fa3895
John Hogan <johnehogan@yahoo.com>
2016-04-24 18:44:32 -0400

    Added van capacity, misc fixes

--- app/models/van_route.rb
+++ app/models/van_route.rb
@@ -3,7 +3,7 @@ class VanRoute < ActiveRecord::Base
   belongs_to :driver
   has_many :route_stops, dependent: :destroy
   accepts_nested_attributes_for :route_stops, :allow_destroy => true
-  default_scope { order('van_routes.name') }
+  default_scope { order('van_routes.name', 'van_routes.am_pm') }
 
   def clone_with_associations(copy_to_date)
     new_van_route = self.dup
