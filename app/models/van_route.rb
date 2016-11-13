6738e895b37299a4b879ac150f5de9696ab41300
John Hogan <jhogan@sagecloud.com>
2016-11-13 07:38:13 -0500

    Fixed van route copy

--- app/models/van_route.rb
+++ app/models/van_route.rb
@@ -12,8 +12,7 @@ class VanRoute < ActiveRecord::Base
     new_van_route.route_date = copy_to_date
 
     self.route_stops.each do |route_stop|
-      new_route_stop = route_stop.clone
-      new_route_stop.placement = route_stop.placement
+      new_route_stop = route_stop.dup
       new_route_stop.save
       new_van_route.route_stops << new_route_stop
     end
