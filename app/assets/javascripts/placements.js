bbeb9792da7f609d5f3b15c20214c6901a52d052
John Hogan <jhogan@sagecloud.com>
2017-03-01 11:58:18 -0500

    Fixed sort order of placements

--- app/assets/javascripts/placements.js
+++ app/assets/javascripts/placements.js
@@ -51,7 +51,7 @@ angular.module('app').controller("PlacementController", function($http, $timeout
     vm.setClickedStudent = setClickedStudent;
     vm.showOrganizationDetails = showOrganizationDetails;
     vm.showResultAlert = false;
-    vm.sortReverse = false;
+    vm.sortReverse = true;
     vm.sortType = 'start_date';
     vm.studentPlacements = [];
     vm.studentCount = 0;
